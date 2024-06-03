5947a8ad39ff6ffa67f7ed3af2904a91a7d406fa
REKHA GUPTA <REKHA GUPTA@DESKTOP-8RNOGRT>
2024-06-03 15:06:09 +0530

    constructor added in new third party page

--- GUISeleniumFramework/src/main/java/com/dolibarr/erp/objectrepositoryutility/NewThirdPartyPage.java
+++ GUISeleniumFramework/src/main/java/com/dolibarr/erp/objectrepositoryutility/NewThirdPartyPage.java
@@ -1,11 +1,20 @@
 package com.dolibarr.erp.objectrepositoryutility;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class NewThirdPartyPage
 {
+	WebDriver driver;
+	public NewThirdPartyPage(WebDriver driver) 
+	{
+		this.driver= driver;
+		PageFactory.initElements(driver, this); 
+	}
+	
 	@FindBy (id="name")
 	private WebElement ThirdPartyNameTextField;
 	
