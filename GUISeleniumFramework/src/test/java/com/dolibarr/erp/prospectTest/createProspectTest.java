2d67c8fa6739d52699df5d1db9ff9fa49ba3f24d
REKHA GUPTA <REKHA GUPTA@DESKTOP-8RNOGRT>
2024-06-04 18:24:13 +0530

    ...........

--- GUISeleniumFramework/src/test/java/com/dolibarr/erp/prospectTest/createProspectTest.java
+++ GUISeleniumFramework/src/test/java/com/dolibarr/erp/prospectTest/createProspectTest.java
@@ -1,4 +1,5 @@
 package com.dolibarr.erp.prospectTest;
+
 /**
  * @author anusha
  */
@@ -9,36 +10,39 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.dolibarr.erp.generic.basetest.BaseClass;
-import com.dolibarr.erp.objectrepositoryutility.CommercialPageInfoPage;
+import com.dolibarr.erp.objectrepositoryutility.CommercialInfoPage;
+
 import com.dolibarr.erp.objectrepositoryutility.HomePage;
+import com.dolibarr.erp.objectrepositoryutility.NewCommercialProposalPage;
 import com.dolibarr.erp.objectrepositoryutility.NewThirdPartyPage;
 import com.dolibarr.erp.objectrepositoryutility.Third_PartiesPage;
-import com.dolibarr.erp.objectrepositoryutility.newCommercialProposalPage;
+
 import com.dolibarr.erp.objectrepositoryutility.prosepectInfoTest;
 import com.dolibarr.erp.objectrepositoryutility.prospectPage;
+
 /**
  * This method used to create prospect
  */
 public class createProspectTest extends BaseClass {
 	@Test
 	public void createProspect() throws Throwable {
-		String PName=eLib.getDataFromExcel("ThirdParty", 4, 2)+jLib.getRandomNumber();
-		String city=eLib.getDataFromExcel("ThirdParty", 4, 3);
-		String refcus=eLib.getDataFromExcel("ThirdParty",4,3);
-		String date=jLib.getRequriedDateYYYYDDMM(15);
-		String statusmsg=eLib.getDataFromExcel("ThirdParty",4,3);
-		String statusmsg1=eLib.getDataFromExcel("ThirdParty",4,3);
-		String discription=eLib.getDataFromExcel("ThirdParty",4,3);
-		String Nprice=eLib.getDataFromExcel("ThirdParty",4,3);
-		String PwithTax=eLib.getDataFromExcel("ThirdParty",4,3);
-		String qty=eLib.getDataFromExcel("ThirdParty",4,3);
-		String dis=eLib.getDataFromExcel("ThirdParty",4,3);
-		
-		HomePage h=new HomePage(driver);
+		String PName = eLib.getDataFromExcel("ThirdParty", 4, 2) + jLib.getRandomNumber();
+		String city = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String refcus = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String date = jLib.getRequriedDateYYYYDDMM(15);
+		String statusmsg = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String statusmsg1 = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String discription = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String Nprice = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String PwithTax = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String qty = eLib.getDataFromExcel("ThirdParty", 4, 3);
+		String dis = eLib.getDataFromExcel("ThirdParty", 4, 3);
+
+		HomePage h = new HomePage(driver);
 		h.getThirdPartiesMenu().click();
-		Third_PartiesPage t=new Third_PartiesPage(driver);
+		Third_PartiesPage t = new Third_PartiesPage(driver);
 		t.getNewProspectLink().click();
-		NewThirdPartyPage ntp=new NewThirdPartyPage(driver);
+		NewThirdPartyPage ntp = new NewThirdPartyPage(driver);
 		ntp.getThirdPartyNameTextField().sendKeys(PName);
 		Thread.sleep(2000);
 		ntp.getSelectCustomerProspect().click();
@@ -47,19 +51,18 @@ public class createProspectTest extends BaseClass {
 		Thread.sleep(2000);
 		ntp.getCityTextField().sendKeys(city);
 		ntp.getCreateThirdPartyButton().click();
-		prosepectInfoTest pi=new prosepectInfoTest(driver);
+		prosepectInfoTest pi = new prosepectInfoTest(driver);
 		pi.getProspectInfo().click();
-		prospectPage pp=new prospectPage(driver);
+		prospectPage pp = new prospectPage(driver);
 		pp.getCreateProposal().click();
-		newCommercialProposalPage ncpp=new newCommercialProposalPage(driver);
+		NewCommercialProposalPage ncpp = new NewCommercialProposalPage(driver);
 		ncpp.createProposal(refcus, date);
-		CommercialPageInfoPage cpip =new CommercialPageInfoPage(driver);
-		String actmsg=cpip.getStatus().getText();
-		Assert.assertEquals(actmsg,statusmsg);
-		cpip.addLine(discription, Nprice, qty, dis); 
-		String actMsg1=cpip.getValidateafter().getText();
-		Assert.assertEquals(actMsg1,statusmsg1);
+		CommercialInfoPage cpip = new CommercialInfoPage(driver);
+		String actmsg = cpip.getStatus().getText();
+		Assert.assertEquals(actmsg, statusmsg);
+		cpip.addLine(discription, Nprice, qty, dis);
+		String actMsg1 = cpip.getValidateafter().getText();
+		Assert.assertEquals(actMsg1, statusmsg1);
 	}
 
-	
 }
